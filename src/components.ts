587372091b4040095d572f76789bb40c8f9f2d9b
Jacob <jake@madebychalk.com>
2021-04-28 14:40:13 -0600

    initial commit

---
+++ src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'shrimp-components';
+
+import { defineCustomElements } from 'shrimp-components/loader';
+
+defineCustomElements();
+export const ShrimpHeader = /*@__PURE__*/createReactComponent<JSX.ShrimpHeader, HTMLShrimpHeaderElement>('shrimp-header');
